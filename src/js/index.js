1b0929c9f5b4e5dc6888ca5346f4004c79f8da5e
Joplin Cruz <145364090+JoplinCruz@users.noreply.github.com>
2024-06-08 22:34:18 -0300

    main page projeto star devs
    paródia de personagens do filme star wars numa versão desenvolvedor front end

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0
+
+
+btnAvancar.addEventListener("click", function () {
+
+    if (cartaoAtual === cartoes.length - 1) return;
+
+    esconderCartaoSelecionado();
+    cartaoAtual++;
+    mostrarCartao();
+
+});
+
+
+btnVoltar.addEventListener("click", function () {
+
+    if (cartaoAtual == 0) return;
+
+    esconderCartaoSelecionado();
+    cartaoAtual--;
+    mostrarCartao();
+
+});
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
